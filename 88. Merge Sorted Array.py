d52e9495a2ced6fed0880babbd21719a8bd8fed9
DavidPav123 <davidpavlicek123@gmail.com>
2022-11-05 20:15:34 -1000

    Finished Q 88

--- 88. Merge Sorted Array.py
+++ 88. Merge Sorted Array.py
@@ -1,8 +1,10 @@
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        if n == 0: return m
+        if n == 0:
+            return m
         else:
-            for nums in enumerate(nums2):
-                nums1[m + nums[0]] == nums2[1]
+            for nums in nums2:
+                nums1.append(nums)
+                nums1.remove(0)
             nums1.sort()
 
